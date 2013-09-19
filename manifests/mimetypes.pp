7df7cb938210690a6a60c3eda6dff26c15a4b9b9
Goran Miskovic <goran.miskovic@arbor-education.com>
2013-09-19 17:20:29 +0200

    Executing command as a root.

--- manifests/mimetypes.pp
+++ manifests/mimetypes.pp
@@ -14,11 +14,15 @@ class nginx::mimetypes ($mime_types) {
 
 # Remove blank lines from the end of mime.types
   exec {'blank':
-    command => "sed -e :a -e '/^\n*$/{$d;N;ba' -e '}' ${file} > ${file}.tmp"
+    command => "sed -e :a -e '/^\n*$/{$d;N;ba' -e '}' ${file} > ${file}.tmp",
+    user    => 'root',
+    group   => 'root'
   }
 # Remove the last line of mime.types having closing curly bracket
   exec {'last':
-    command => "sed '$d' ${file}.tmp > ${file}"
+    command => "sed '$d' ${file}.tmp > ${file}",
+    user    => 'root',
+    group   => 'root'
   }
 # Add new types
   file { 'tmp-one':
@@ -28,12 +32,16 @@ class nginx::mimetypes ($mime_types) {
   }
 # Concatenate files
   exec {'concat':
-    command => "cat ${file}.tmp >> ${file}"
+    command => "cat ${file}.tmp >> ${file}",
+    user    => 'root',
+    group   => 'root'
   }
 # Clean up
   exec { 'tmp-two':
     command => "rm ${file}.tmp",
-    notify => Service['nginx']
+    notify => Service['nginx'],
+    user    => 'root',
+    group   => 'root'
   }
 
   Exec['blank'] -> Exec['last'] -> File['tmp-one'] -> Exec['concat']
