9144679fab9c0630f5d20de6ffd747bb1c0edeaf
Goran Miskovic <goran.miskovic@arbor-education.com>
2013-09-20 18:51:11 +0200

    Correcting problem with new line character and using tee instead of pipe

--- manifests/mimetypes.pp
+++ manifests/mimetypes.pp
@@ -1,50 +1,44 @@
 class nginx::mimetypes ($mime_types) {
 
-  $file = "${nginx::params::nx_conf_dir}/mime.types"
+$file = "${nginx::params::nx_conf_dir}/mime.types"
 
-  Exec {
-    path => [
-      '/bin/',
-      '/sbin/' ,
-      '/usr/bin/',
-      '/usr/sbin/',
-      '/usr/local/bin/'
-    ]
-  }
+Exec {
+path => [
+'/bin/',
+'/sbin/' ,
+'/usr/bin/',
+'/usr/sbin/',
+'/usr/local/bin/'
+]
+}
 
 # Remove blank lines from the end of mime.types
-  exec {'blank':
-    command => "sed -e :a -e '/^\n*$/{$d;N;ba' -e '}' ${file} > ${file}.tmp",
-    user    => 'root',
-    group   => 'root'
-  }
+exec {'blank':
+  command => "sed -e :a -e '|^\'$'\n*$|{$d;N;ba' -e '}' ${file} | sudo tee ${file}.tmp",
+}
 # Remove the last line of mime.types having closing curly bracket
-  exec {'last':
-    command => "sed '$d' ${file}.tmp > ${file}",
-    user    => 'root',
-    group   => 'root'
-  }
+exec {'last':
+  command => "sed '$d' ${file}.tmp | sudo tee ${file}",
+}
 # Add new types
-  file { 'tmp-one':
-    path => "${file}.tmp",
-    ensure => 'file',
-    content => template('nginx/conf.d/mime.types.erb')
-  }
+file { 'tmp-one':
+  path => "${file}.tmp",
+  ensure => 'file',
+  content => template('nginx/conf.d/mime.types.erb')
+}
 # Concatenate files
-  exec {'concat':
-    command => "cat ${file}.tmp >> ${file}",
-    user    => 'root',
-    group   => 'root'
-  }
+exec {'concat':
+  command => "cat ${file}.tmp | sudo tee -a ${file}",
+}
 # Clean up
-  exec { 'tmp-two':
-    command => "rm ${file}.tmp",
-    notify => Service['nginx'],
-    user    => 'root',
-    group   => 'root'
-  }
+exec { 'tmp-two':
+  command => "rm ${file}.tmp",
+  notify => Service['nginx'],
+  user    => 'root',
+  group   => 'root'
+}
 
-  Exec['blank'] -> Exec['last'] -> File['tmp-one'] -> Exec['concat']
-    -> Exec['tmp-two']
+Exec['blank'] -> Exec['last'] -> File['tmp-one'] -> Exec['concat']
+-> Exec['tmp-two']
 
 }
